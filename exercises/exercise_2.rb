6c2ccc91024602003eb4370849c34fa3f5e5c435
Alexander Horvath <tantousha@me.com>
2018-09-11 17:14:58 -0700

    Answered exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,8 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@Store1 = Store.where(id: 1).first
+@Store2 = Store.where(id: 2).first
+@Store1.name = "Langley"
+
+puts "Store 1 is now called #{@Store1.name}"
